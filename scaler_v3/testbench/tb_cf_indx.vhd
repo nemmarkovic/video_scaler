a71eed7f5ab898ac621e06a566c0369600049e04
Nebojsa Markovic <nebojsa.markovic@rt-rk.com>
2021-11-23 19:49:36 +0100

    ppsspsps

--- scaler_v3/testbench/tb_cf_indx.vhd
+++ scaler_v3/testbench/tb_cf_indx.vhd
@@ -10,8 +10,8 @@ library common_lib;
 
 entity tb_cf_indx is
      generic(
-        G_IN_SIZE       : integer               :=  16;
-        G_OUT_SIZE      : integer               :=  4;
+        G_IN_SIZE       : integer               :=  4;
+        G_OUT_SIZE      : integer               :=  21;
         G_PHASE_NUM     : integer range 2 to 64 :=    4;
         G_DWIDTH        : integer range 1 to 64 :=    8);
 end;
@@ -21,10 +21,15 @@ architecture bench of tb_cf_indx is
   signal i_clk            : std_logic;
   signal i_rst            : std_logic;
   signal i_valid          : std_logic;
-  signal o_ready          : std_logic;
   signal i_pos            : std_logic_vector(11 -1 downto 0);
   signal i_ready_indx     : std_logic;
+  signal o_ready          : std_logic;
   signal o_cf             : t_cf_indx_array;
+
+  signal l_valid          : std_logic;
+  signal l_pos            : std_logic_vector(11 -1 downto 0);
+  signal l_ready_indx     : std_logic;
+
    constant clk_period : time := 50 ns;
 begin
 
@@ -57,36 +62,41 @@ rst_proc: process
          i_rst <= '1';
       wait for clk_period *10;  
          i_rst <= '0';
+              wait for clk_period *100; 
       wait;  
    end process;
+
+  stimulus_comb: process(all)
+   begin
+      l_ready_indx <= i_ready_indx;
+      l_valid <= i_valid;
+      l_pos   <= i_pos;   
+      if (l_valid and o_ready) = '1' and to_integer(unsigned(i_pos)) < G_IN_SIZE -1  then
+        l_pos   <= std_logic_vector(unsigned(i_pos) + 1);
+        l_valid <= '1';
+      elsif o_ready <= '1' then
+        l_valid <= '1';
+      end if;
+   end process;
+
    
   stimulus: process(i_clk)
       variable vr_start : std_logic;
    begin
       if rising_edge(i_clk) then
          if i_rst = '1' then
-            i_valid          <= '0';
-            i_pos            <= (others => '0');--: std_logic_vector(11 -1 downto 0);
-            --i_start_pos      <= (others => '0');--: std_logic_vector(11 -1 downto 0);
-            i_ready_indx     <= '0';--: std_logic_vector(0 to G_PHASE_NUM -1);        
-            vr_start         := '1';
-         else
             i_ready_indx     <= '0';
-
-            if (i_valid  and   o_ready) = '1' then
-              -- i_valid <= '0';
- 
-             if vr_start = '1' and to_integer(unsigned(i_pos)) < G_IN_SIZE -1 then
-               i_pos <= std_logic_vector(unsigned(i_pos) +1);
-             end if;
- 
-             vr_start         := '1';
-            elsif(o_ready = '1') then
-               i_valid <= '1';         
-            else
-               i_valid <= i_valid;            
+            i_valid          <= '0';
+            i_pos            <= (others => '0');
+            vr_start         := '0';
+         else
+            i_pos   <= l_pos;
+            i_valid <= l_valid;
+            i_ready_indx <= l_ready_indx;
+            if vr_start = '0' then
+               i_ready_indx <= '1';
+               vr_start     := '1';
             end if;
-
          end if;
       end if;
    end process;
